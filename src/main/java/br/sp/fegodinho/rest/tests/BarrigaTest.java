15f64fc74e8f4177661d0e7da4c5184feed4c09b
Felipe Godinho <fegodinho@gmail.com>
2020-07-22 18:06:40 -0300

    Aula 55 - Deve incluir conta com sucesso

--- src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
+++ src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
@@ -2,6 +2,9 @@ package br.sp.fegodinho.rest.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.sp.fegodinho.rest.core.BaseTest;
@@ -16,6 +19,33 @@ public class BarrigaTest extends BaseTest {
 		.then()
 			.statusCode(401)
 		;
+	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "fegodinho@godinho");
+		login.put("senha", "123456");
+		
+		//obtendo token de acesso
+		String token =  given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+		;
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{\"nome\": \"conta qualquer\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+		
 	}
 
 }
