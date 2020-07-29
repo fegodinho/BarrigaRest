bee75fc600611bcc45f032752dcdf686524bdbff
Felipe Godinho <fegodinho@gmail.com>
2020-07-29 10:05:01 -0300

    Aula 56 - Deve alterar conta com sucesso

--- src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
+++ src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
@@ -1,15 +1,36 @@
 package br.sp.fegodinho.rest.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.sp.fegodinho.rest.core.BaseTest;
 
 public class BarrigaTest extends BaseTest {
+	
+	private String TOKEN;
+	
+	@Before
+	public void login() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "fegodinho@godinho");
+		login.put("senha", "123456");
+		
+		//obtendo token de acesso
+		TOKEN =  given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+	}
 	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
@@ -22,28 +43,31 @@ public class BarrigaTest extends BaseTest {
 	}
 	
 	@Test
-	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap<String, String>();
-		login.put("email", "fegodinho@godinho");
-		login.put("senha", "123456");
-		
-		//obtendo token de acesso
-		String token =  given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token");
-		;
+	public void deveIncluirContaComSucesso() {	
 		
 		given()
-			.header("Authorization", "JWT " + token)
+			.header("Authorization", "JWT " + TOKEN)
 			.body("{\"nome\": \"conta qualquer\"}")
 		.when()
 			.post("/contas")
 		.then()
 			.statusCode(201)
+		;
+		
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {		
+		
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"conta alterada\"}")
+		.when()
+			.put("/contas/221069")
+		.then()
+			.statusCode(200)
+			.log().all()
+			.body("nome", is("conta alterada"))
 		;
 		
 	}
