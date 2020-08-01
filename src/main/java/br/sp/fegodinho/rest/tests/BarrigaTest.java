cb4aab7e4f583e954b19a697e11a3412508611a7
Felipe Godinho <fegodinho@gmail.com>
2020-08-01 11:59:34 -0300

    Aula 65 - Gerenciamento de massa, parte 2

--- src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
+++ src/main/java/br/sp/fegodinho/rest/tests/BarrigaTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
---
+++ src/main/java/br/sp/fegodinho/rest/tests/refac/ContasTest.java
@@ -0,0 +1,35 @@
+package br.sp.fegodinho.rest.tests.refac;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+
+public class ContasTest {
+	
+	@BeforeClass
+	public static void login() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "fegodinho@godinho");
+		login.put("senha", "123456");
+		
+		//obtendo token de acesso
+		String TOKEN =  given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		RestAssured.requestSpecification.header("Authorization", "JWT " + TOKEN);
+		
+		RestAssured.get("/reset").then().statusCode(200);
+	}	
+
+}
