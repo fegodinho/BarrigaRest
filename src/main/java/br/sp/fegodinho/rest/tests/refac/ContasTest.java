e5426d6cdf8a6e3e3f2b3cedff65a0e702af6bfa
Felipe Godinho <fegodinho@gmail.com>
2020-08-03 10:50:35 -0300

    Aula 66 - Teste de Contas

--- src/main/java/br/sp/fegodinho/rest/tests/refac/ContasTest.java
+++ src/main/java/br/sp/fegodinho/rest/tests/refac/ContasTest.java
@@ -1,15 +1,18 @@
 package br.sp.fegodinho.rest.tests.refac;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import br.sp.fegodinho.rest.core.BaseTest;
 import io.restassured.RestAssured;
 
-public class ContasTest {
+public class ContasTest extends BaseTest{
 	
 	@BeforeClass
 	public static void login() {
@@ -30,6 +33,52 @@ public class ContasTest {
 		RestAssured.requestSpecification.header("Authorization", "JWT " + TOKEN);
 		
 		RestAssured.get("/reset").then().statusCode(200);
-	}	
+	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {	
+		given()
+			.body("{\"nome\": \"Conta inserida\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;		
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {	
+		
+		Integer CONTA_ID = getIdContaPeloNome("Conta para alterar");
+		
+		given()
+			.body("{\"nome\": \"Conta alterada\"}")
+			.pathParam("id", CONTA_ID)
+		.when()
+			.put("/contas/{id}")
+		.then()
+			.statusCode(200)
+			.body("nome", is("Conta alterada"))
+		;
+		
+	}
+	
+	@Test
+	public void naoDeveIncluirContaComMesmoNome() {	
+		
+		given()
+			.body("{\"nome\": \"Conta mesmo nome\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+		;
+		
+	}
+	
+	public Integer getIdContaPeloNome(String nome) {
+		return RestAssured.get("/contas?nome="+nome).then().extract().path("id[0]");
+	}
 
 }
