5e32563a5fb3839f487993d78d5a09360da8ef2e
Felipe Godinho <fegodinho@gmail.com>
2020-08-03 11:13:04 -0300

    Aula 67 - Teste de Movimentacao

---
+++ src/main/java/br/sp/fegodinho/rest/tests/refac/MovimentacaoTest.java
@@ -0,0 +1,145 @@
+package br.sp.fegodinho.rest.tests.refac;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.sp.fegodinho.rest.core.BaseTest;
+import br.sp.fegodinho.rest.tests.Movimentacao;
+import br.sp.fegodinho.rest.utils.DataUtils;
+import io.restassured.RestAssured;
+
+public class MovimentacaoTest extends BaseTest{
+	
+	@BeforeClass
+	public static void login() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "fegodinho@godinho");
+		login.put("senha", "123456");
+		
+		//obtendo token de acesso
+		String TOKEN =  given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		RestAssured.requestSpecification.header("Authorization", "JWT " + TOKEN);
+		
+		RestAssured.get("/reset").then().statusCode(200);
+	}
+	
+	@Test
+	public void deveInserirMovimentacaoComSucesso() {
+		Movimentacao mov = getMovimentacaoValida();
+		
+		given()
+			.body(mov)
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(201)
+		;
+		
+	}
+	
+	@Test
+	public void deveValidarCamposObrigatoriosNaMovimentacao() {
+				
+		given()
+			.body("{}")
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(8))
+			.body("msg", hasItems(
+					"Data da Movimentação é obrigatório",
+					"Data do pagamento é obrigatório",
+					"Descrição é obrigatório",
+					"Interessado é obrigatório",
+					"Valor é obrigatório",
+					"Valor deve ser um número",
+					"Conta é obrigatório",
+					"Situação é obrigatório"
+					))
+		;
+		
+	}
+	
+	@Test
+	public void naoDeveInserirMovimentacaoComDataFutura() {
+		Movimentacao mov = getMovimentacaoValida();
+		mov.setData_transacao(DataUtils.GetDataDiferencaDias(2));
+		
+		given()
+			.body(mov)
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(1))
+			.body("msg", hasItems("Data da Movimentação deve ser menor ou igual à data atual"))
+		;
+		
+	}
+	
+	@Test
+	public void naoDeveRemoverContaComMovimentacao() {
+		Integer CONTA_ID = getIdContaPeloNome("Conta com movimentacao");
+		
+		given()
+			.pathParam("id", CONTA_ID)
+		.when()
+			.delete("/contas/{id}")
+		.then()
+			.statusCode(500)
+			.body("constraint", is("transacoes_conta_id_foreign"))
+		;
+		
+	}
+	
+	@Test
+	public void deveRemoverMovimentacao() {
+		Integer MOV_ID = getIdMovPelaDescricao("Movimentacao para exclusao");
+		given()
+			.pathParam("id", MOV_ID)
+		.when()
+			.delete("/transacoes/{id}")
+		.then()
+			.statusCode(204)
+		;
+		
+	}	
+	
+	public Integer getIdContaPeloNome(String nome) {
+		return RestAssured.get("/contas?nome="+nome).then().extract().path("id[0]");
+	}
+	
+	public Integer getIdMovPelaDescricao(String desc) {
+		return RestAssured.get("/transacoes?descricao="+desc).then().extract().path("id[0]");
+	}
+	
+	private Movimentacao getMovimentacaoValida() {
+		Movimentacao mov = new Movimentacao();
+		mov.setConta_id(getIdContaPeloNome("Conta para movimentacoes"));
+		//mov.setUsuario_id(usuario_id);
+		mov.setDescricao("Descricao da movimentacao");
+		mov.setEnvolvido("Envovido na movimentacao");
+		mov.setTipo("REC");
+		mov.setData_transacao(DataUtils.GetDataDiferencaDias(-1));
+		mov.setData_pagamento(DataUtils.GetDataDiferencaDias(5));
+		mov.setValor(100f);
+		mov.setStatus(true);
+		return mov;
+	}
+
+}
